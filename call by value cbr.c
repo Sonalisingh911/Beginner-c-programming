3b72655c39caf5d990c8ea944c311713e6b7aab7
Sonalisingh911 <59063897+Sonalisingh911@users.noreply.github.com>
2019-12-20 01:18:04 +0530

    Add files via upload
    POINTER

---
+++ call by value cbr.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void swap(int,int);
+int main()
+{int a,b;
+printf("enter a and b");
+scanf("%d%d",&a,&b);
+printf("before swaping a=%d b=%d",a,b);
+swap(a,b);
+printf("after swap a=%d b=%d",a,b);
+return 0;
+}
+void swap(int a,int b)
+{int t;
+t=a;
+a=b;
+b=t;
+}
